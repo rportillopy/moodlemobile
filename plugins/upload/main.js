9311eccf0686b0445215518d43604b033785abd7
Isaac Marco Blancas <marcoblancas@gmail.com>
2014-06-02 01:18:09 +0200

    adding .jpg to filename if it don't finish with .jpg

--- plugins/upload/main.js
+++ plugins/upload/main.js
@@ -81,7 +81,10 @@ define(function () {
             } else {
                 options.fileName = uri.substring(uri.lastIndexOf("/") + 1);
             }
-
+            // We add .jpg to the fileName if it don't finish with .jpg in order to be pickable when user change picture profile.
+            if (options.fileName.substring(options.fileName.length-4)) != ".jpg" {
+                options.fileName += ".jpg";
+            }
             options.mimeType="image/jpeg";
 
             MM.moodleUploadFile(uri, options,
